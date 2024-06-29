2ec60ea3185918cf16f239e6590b689d9293d1bf
Benjamin Wright <BenWrightSWE@gmail.com>
2024-06-28 23:30:58 -0400

    Finished the project. It has full responsiveness across 3 breakpoints (4 ranges of media), includes day/night feature, and has transition animations. Still need to lookover to make sure to put any accessibility tags and other smaller features.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,8 @@
+var isDay: boolean = true;
+var root = document.querySelector(':root');
 
 document.getElementById("nav-button").addEventListener("click", openCloseNav);
+document.getElementById("night-mode-button").addEventListener("click", dayNight);
 
 function openCloseNav() {
     let closedNav = document.getElementById("hidden-closed-nav").innerHTML;
@@ -10,5 +13,36 @@ function openCloseNav() {
     } else {
         document.getElementById("nav-button").innerHTML = "&#9776";
         document.getElementById("header-nav").style.display="none";
+    }
+}
+
+function dayNight(){
+    buttonSwitch();
+    colorSwitch();
+}
+
+function buttonSwitch() {
+    if (isDay == true) {
+        document.getElementById("switch-up").style.transform="translateY(-50%)";
+        isDay = false;
+    } else {
+        document.getElementById("switch-up").style.transform="translateY(0%)";
+        isDay = true;
+    }
+}
+
+function colorSwitch() {
+    if (isDay == false) {
+        root.style.setProperty("--bodyBackgroundColor", "#6D5D6E");
+        root.style.setProperty("--headerBackgroundColor", "#393646");
+        root.style.setProperty("--headerNavBackgroundColor", "#4F4557");
+        root.style.setProperty("--headerNavButtonColor", "#424242");
+        root.style.setProperty("--textColor", "#F4EEE0");
+    } else {
+        root.style.setProperty("--bodyBackgroundColor", "#EEE9E3");
+        root.style.setProperty("--headerBackgroundColor", "#C5A586");
+        root.style.setProperty("--headerNavBackgroundColor", "#E7D6C6");
+        root.style.setProperty("--headerNavButtonColor", "#D4D6D5");
+        root.style.setProperty("--textColor", "#474747");
     }
 }
